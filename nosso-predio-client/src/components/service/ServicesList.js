31a6c18429ad0b09ebae48eaa88d1ca7582274de
GabrielBohn99 <gabrielcbohn@gmail.com>
2020-05-21 18:28:30 -0300

    Service improved

--- nosso-predio-client/src/components/service/ServicesList.js
+++ nosso-predio-client/src/components/service/ServicesList.js
@@ -12,12 +12,8 @@ const ServicesList = (props) => {
                 to={`/condominio/${service.building}/serviço/${service._id}`}
               >
                 <div className="card-box">
-                  <img
-                    className="card-img"
-                    src={service.image}
-                    alt='img'
-                  />
-                  <div className='service-card-right-side'>
+                  <img className="card-img" src={service.image} alt="img" />
+                  <div className="service-card-right-side">
                     <h3>{service.name.toUpperCase()}</h3>
                     <p>{service.description}</p>
                   </div>
@@ -30,20 +26,21 @@ const ServicesList = (props) => {
     );
   } else {
     return (
-      <div className="card-container" >
+      <div className="card-container">
         {props.services.map((service, idx) => {
           return (
             <div key={idx} className="card-box">
-              <Link className="card-box-link"
+              <Link
+                className="card-box-link"
                 to={`/condominio/${service.building}/serviço/${service._id}`}
               >
-                  <img
-                    className="card-img"
-                    src={service.image}
-                    alt='img'
-                  />
-                  <h3 className="card-title">{service.name}</h3>
+                <img className="card-img" src={service.image} alt="img" />
+                <h3 className="card-title">{service.name}</h3>
+                {service.price === 0 ? (
+                  <h3 className="card-text">Grátis</h3>
+                ) : (
                   <h3 className="card-text">R${service.price.toFixed(2)}</h3>
+                )}
               </Link>
             </div>
           );
